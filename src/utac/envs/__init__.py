b9ee92943c57c55bc30fc2a76095454f399dff5b
Nathanael Lu <lunathanael@gmail.com>
2025-01-12 03:29:14 -0500

    added basic temp wrapper

--- src/utac/envs/__init__.py
+++ src/utac/envs/__init__.py
@@ -1,10 +1,11 @@
-from . import utacenv, utils
+from . import utacenv, utils, wrappers
 from .utacenv import UtacEnv
 from gymnasium import register
+from .wrappers import InfoWrapper
 
 register(
     id="utac-v0",
     entry_point="utac.envs:UtacEnv",
 )
 
-__all__ = ["UtacEnv"]
+__all__ = ["utacenv", "utils", "wrappers", "UtacEnv", "InfoWrapper"]

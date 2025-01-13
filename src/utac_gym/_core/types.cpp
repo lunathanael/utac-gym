5112e2f5e1b5b27e36d6bd061079388e0dd88ddf
Nathanael Lu <lunathanael@gmail.com>
2025-01-13 01:35:20 -0500

    fix nanobind syntax error

--- src/utac_gym/_core/types.cpp
+++ src/utac_gym/_core/types.cpp
@@ -7,11 +7,11 @@ namespace nb = nanobind;
 
 void bind_types(nb::module_ &m) {
     nb::class_<GAMESTATE>(m, "GameState")
-        .def_readwrite("occ", &GAMESTATE::occ)
-        .def_readwrite("board", &GAMESTATE::board)
-        .def_readwrite("game_occ", &GAMESTATE::game_occ)
-        .def_readwrite("main_occ", &GAMESTATE::main_occ)
-        .def_readwrite("main_board", &GAMESTATE::main_board)
-        .def_readwrite("side", &GAMESTATE::side)
-        .def_readwrite("last_square", &GAMESTATE::last_square);
+        .def_rw("occ", &GAMESTATE::occ)
+        .def_rw("board", &GAMESTATE::board)
+        .def_rw("game_occ", &GAMESTATE::game_occ)
+        .def_rw("main_occ", &GAMESTATE::main_occ)
+        .def_rw("main_board", &GAMESTATE::main_board)
+        .def_rw("side", &GAMESTATE::side)
+        .def_rw("last_square", &GAMESTATE::last_square);
 }
